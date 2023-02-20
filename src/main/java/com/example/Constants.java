054b164a0f3f21e3992c8f83aed5a1ff58a4ea80
tabletka97 <mtabletka.by@gmail.com>
2023-02-20 18:49:32 +0300

    done?

---
+++ src/main/java/com/example/Constants.java
@@ -0,0 +1,11 @@
+package com.example;
+
+public class Constants {
+    public static final String JSP_ADD_JSP = "/jsp/add.jsp";
+    public static final String JSP_USERS_JSP = "/jsp/users.jsp";
+
+    public static final String PARAM_FIRST_NAME = "firstName";
+    public static final String PARAM_LAST_NAME = "lastName";
+    public static final String PARAM_USER = "user";
+    public static final String PARAM_USERS = "users";
+}
--- src/main/java/com/example/servlet/AddUserServlet.java
+++ src/main/java/com/example/servlet/AddUserServlet.java
@@ -1,5 +1,35 @@
 package com.example.servlet;
 
-public class AddUserServlet {
-    //write your code here!
+import com.example.User;
+import com.example.Warehouse;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+import static com.example.Constants.*;
+
+@WebServlet("/add")
+public class AddUserServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+            throws ServletException, IOException {
+        getServletContext().getRequestDispatcher(JSP_ADD_JSP).forward(req, resp);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp)
+            throws ServletException, IOException {
+        String firstName = req.getParameter(PARAM_FIRST_NAME);
+        String lastName = req.getParameter(PARAM_LAST_NAME);
+        User user = new User(firstName, lastName);
+        req.setAttribute(PARAM_USER, user);
+        Warehouse warehouse = Warehouse.getInstance();
+        warehouse.addUser(user);
+        getServletContext().getRequestDispatcher(JSP_ADD_JSP).forward(req, resp);
+    }
 }
